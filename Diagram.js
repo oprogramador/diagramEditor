b44bfe23cf6ea6ef6422609b23ac1c98145e66b5
oprogramador <uomodislesia@gmail.com>
2015-04-30 04:16:24 +0200

    drag

--- Diagram.js
+++ Diagram.js
@@ -1,17 +1,34 @@
 function Diagram(elementId, templateId) {
     this.add = add;
     this.select = select;
-    this.choose = choose;
-    this.mousedown = mousedown;
 
-    var currentGroup;
-    var lastGroup;
+    var currentGroup = null;
+    var lastGroup = null;
     var lastId = 0;
+    var lastX = null;
+    var lastY = null;
 
     $(document.body).on('click', '[id^=svg-g-]', function(e) {
         var id = this.id.split('-')[2];
         console.log('click '+id);
         choose(id);
+    });
+
+    $(document.body).on('mousedown', '[id^=svg-g-]', function(e) {
+        var id = this.id.split('-')[2];
+        mousedown(id, e.clientX, e.clientY);
+    });
+
+    $(document.body).on('mouseup', '#'+elementId, function(e) {
+        mouseup(e.clientX, e.clientY);
+    });
+
+    $(document.body).on('mousemove', '#'+elementId, function(e) {
+        mousemove(e.clientX, e.clientY);
+    });
+
+    $(document.body).on('dragstart', '[id^=svg-g-]', function(e) {
+        return false;
     });
 
     function add() {
@@ -21,13 +38,12 @@ function Diagram(elementId, templateId) {
         el.innerHTML = template.innerHTML;
         lastId++;
         el.id = 'svg-g-'+lastId;
-        console.log('id='+lastId);
         element.appendChild(el);
         refresh();
     }
 
     function select() {
-        element.attr('cursor', 'pointer');
+        $('#'+elementId).attr('cursor', 'move');
     }
 
     function choose(id) {
@@ -35,8 +51,38 @@ function Diagram(elementId, templateId) {
         currentGroup = id;
     }
 
-    function mousedown(e) {
-        console.log(' x='+e.clientX+' y='+e.clientY);
+    function mousedown(id, x, y) {
+        lastGroup = id;
+        lastX = x;
+        lastY = y;
+        console.log('id='+id+' x='+x+' y='+y);
+    }
+
+    function mouseup(x, y) {
+        console.log('up x='+x+' y='+y);
+        if(lastX !== null) moveGroup(lastGroup, x-lastX, y-lastY);
+        lastGroup = null;
+        lastX = null;
+        lastY = null;
+    }
+
+    function mousemove(x, y) {
+        //console.log('move x='+x+' y='+y);
+        //console.log('lastX='+lastX);
+        if(lastX !== null) {
+            moveGroup(lastGroup, x-lastX, y-lastY);
+            lastX = x;
+            lastY = y;
+        }
+    }
+
+    function moveGroup(id, dx, dy) {
+        var children = $('#svg-g-'+lastGroup).children();
+        console.log('dx='+dx+' dy='+dy);
+        for(var i=0; i<children.length; i++) {
+            children[i].setAttribute('x', parseInt(children[i].getAttribute('x'))+dx);
+            children[i].setAttribute('y', parseInt(children[i].getAttribute('y'))+dy);
+        }
     }
 
     function refresh() {
