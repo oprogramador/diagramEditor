a044f06bfcf827ed0785026f9f4c122df864e0a8
oprogramador <uomodislesia@gmail.com>
2015-04-30 03:05:47 +0200

    events

--- Diagram.js
+++ Diagram.js
@@ -1,11 +1,42 @@
-function Diagram(id) {
-    this.id = id;
+function Diagram(elementId, templateId) {
     this.add = add;
-    var element = $('#'+id);
+    this.select = select;
+    this.choose = choose;
+    this.mousedown = mousedown;
+
+    var currentGroup;
+    var lastGroup;
+    var lastId = 0;
+
+    $(document.body).on('click', '[id^=svg-g-]', function(e) {
+        var id = this.id.split('-')[2];
+        console.log('click '+id);
+        choose(id);
+    });
 
     function add() {
-        element.append('<rect xmlns="http://www.w3.org/2000/svg" width="200" height="100"></rect>');
-        refresh()
+        var template = $('#'+templateId)[0];
+        var element = $('#'+elementId)[0];
+        var el = template.cloneNode();
+        el.innerHTML = template.innerHTML;
+        lastId++;
+        el.id = 'svg-g-'+lastId;
+        console.log('id='+lastId);
+        element.appendChild(el);
+        refresh();
+    }
+
+    function select() {
+        element.attr('cursor', 'pointer');
+    }
+
+    function choose(id) {
+        console.log($('#svg-g-'+id).html());
+        currentGroup = id;
+    }
+
+    function mousedown(e) {
+        console.log(' x='+e.clientX+' y='+e.clientY);
     }
 
     function refresh() {
